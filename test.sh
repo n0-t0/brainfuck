07233c580c6aa4919ce4afb3aca5eaf651d19ec1
Tomohiro Nishimura <52896841+n0-t0@users.noreply.github.com>
2024-04-01 10:13:12 +0900

    Update test.sh
    cleanup test.sh

--- test.sh
+++ test.sh
@@ -1,10 +1,6 @@
 #!/bin/zsh
 
-# Scalaファイルからネイティブ実行可能ファイルを作成
 scala-cli package -o executable --native -f bf.scala
-
 echo "start"
-# `testcase.txt`の内容を引数としてexecutableを実行
-# 空文字列を第二引数として渡す
 time ./executable "$(< testcase.txt)" ""
 
